809c5f698160fad5dc1fe9f1d552a8819abd9911
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2019-12-14 15:18:32 -0500

    Added make file

--- main.cpp
+++ main.cpp
@@ -7,38 +7,38 @@ int main() {
     auto *listenerLibrary = new ListenerLibrary<int>();
 
     listenerLibrary->add_function(0, [](int a) {
-        std::cout << "Function 1 with id: " << a << std::endl;
+        printf("Function 1 with id: %d\n", a);
     });
     listenerLibrary->add_function(0, [](int a) {
-        std::cout << "Function 2 with id: " << a << std::endl;
+        printf("Function 2 with id: %d\n", a);
     });
     listenerLibrary->add_function(0, [](int a) {
-        std::cout << "Function 3 with id: " << a << std::endl;
+        printf("Function 3 with id: %d\n", a);
     });
 
     listenerLibrary->add_function(1, [](int a) {
-        std::cout << "Function 1 with id: " << a << std::endl;
+        printf("Function 1 with id: %d\n", a);
     });
     listenerLibrary->add_function(1, [](int a) {
-        std::cout << "Function 2 with id: " << a << std::endl;
+        printf("Function 2 with id: %d\n", a);
     });
 
 
-    //printf("Waiting for nodes with caller_id 0\n");
+    printf("Waiting for nodes with caller_id 0\n");
 
     listenerLibrary->notify_listeners(0, 0);
 
-    //listenerLibrary->wait();
+    listenerLibrary->wait();
 
-    //printf("Finished waiting on nodes with caller_id 0\n");
+    printf("Finished waiting on nodes with caller_id 0\n");
 
-    //printf("Waiting for nodes with caller_id 1\n");
+    printf("Waiting for nodes with caller_id 1\n");
 
     listenerLibrary->notify_listeners(1, 1);
 
     listenerLibrary->wait();
 
-    //printf("Finished waiting on nodes with caller_id 1\n");
+    printf("Finished waiting on nodes with caller_id 1\n");
 
     delete listenerLibrary;
 
